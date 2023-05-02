003d1589d8a3dd30b912675abedfb84dc78fdb6d
MarkLambson <mj.lambson@yahoo.com>
2023-05-02 11:57:46 -0700

    Super Ninja

---
+++ Ninja/sensei.mjs
@@ -0,0 +1,20 @@
+import Ninja from "./ninjs.mjs";
+
+class Sensei extends Ninja {
+//only pass into constructor the name!! set default values for other attributes
+    constructor(name) {
+        super(name);
+        this.health = 200;
+        this.speed = 10;
+        this.strength = 10;
+        this.wisdom = 10;
+    }
+//methods
+    speakWisdom () {
+        return super.drinkSake() + console.log("If the shoe fits, it must be your size...");
+    }
+}
+
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+superSensei.showStats();
